d2bc5a5d7b84ca3d75241e149ea5177beca2a67b
Pavel Kazanin <pkazanin@yandex.ru>
2023-11-12 15:10:56 +0300

    Actual version

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(apiLimiter);
 
 const corsOptions = {
-  origin: ['http://pavelkazaninmovies.nomoredomainsrocks.ru', 'https://pavelkazaninmovies.nomoredomainsrocks.ru', 'http://localhost:3000'],
+  origin: ['http://pavelkazaninmovies.nomoredomainsmonster.ru', 'https://pavelkazaninmovies.nomoredomainsmonster.ru', 'http://localhost:3000'],
   optionsSuccessStatus: 200,
   credentials: true,
 };
